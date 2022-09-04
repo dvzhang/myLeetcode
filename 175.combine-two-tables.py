7201886a3ad9c4c73cc5150db70f296251d7fb0f
dvzhang <dvzhangtz@sina.com>
2022-09-04 09:19:34 -0700

    9.4 comment

--- 175.combine-two-tables.py
+++ 175.combine-two-tables.py
@@ -1,4 +1,17 @@
+# select p.firstName, p.lastName, a.city, a.state
+# from person p
+# left join address a
+# on p.personId=a.personId
+
+import numpy as np
+import pandas as pd
+
+person = pd.DataFrame({"PersonId":[1], "LastName":["wang"], "FirstName":["Allen"]})
+address = pd.DataFrame({"PersonId":[1], "AddressId":[2], "City":["NY"]})
+
+ans = person.merge(address, how="left", on="PersonId")[["PersonId", "LastName", "FirstName", "City"]]
 
 
 
+print(ans)
 
