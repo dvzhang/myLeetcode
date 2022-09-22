9492d64cdf756f8a72ce5e5e071d05eeb19fa727
dvzhang <dvzhangtz@sina.com>
2022-09-22 00:59:15 -0700

    9.22 comment 2

--- 27.remove-element.py
+++ 27.remove-element.py
@@ -6,7 +6,25 @@
 
 # @lc code=start
 class Solution:
-    def removeElement(self, nums: List[int], val: int) -> int:
+    def removeElement(self, nums: list[int], val: int) -> int:
+        slow = 0
+        for quick in nums:
+            if quick != val:
+                nums[slow] = quick
+                slow += 1
+        return slow
+        # return nums
+
         
 # @lc code=end
 
+import time
+time1 = time.time()
+
+n = [3,2,2,3]
+val = 3
+pro = Solution()
+print(pro.removeElement(n, val))
+
+time2 = time.time()
+print(time2-time1)
